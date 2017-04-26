35f06982edecc23937abe2582dab1158d39c178a
Alan Voss <avoss@rent.com>
2017-04-25 22:17:43 -0500

    adding some additional board terminal printing features

--- lib/connect_four/board.ex
+++ lib/connect_four/board.ex
@@ -8,29 +8,95 @@ defmodule ConnectFour.Board do
     [1, 1, 2, 1, 1, 2, 1],
   ]
 
-  def print(board \\ @board) do
+  @contender_colors %{
+    1 => IO.ANSI.cyan,
+    2 => IO.ANSI.red
+  }
+
+  @contender_characters %{
+    0 => ["   "],
+    1 => [@contender_colors[1], " \xF0\x9F\x85\xB0 "],
+    2 => [@contender_colors[2],  " \xF0\x9F\x85\xB1 "]
+  }
+
+  def new do
+    for i <- 1..7 do
+      for j <- 1..6 do
+        0
+      end
+    end
+  end
+
+  def flip(board) do
+    for row <- board do
+      for column <- row do
+        rem(column + 1, 2)
+      end
+    end
+  end
+
+  def drop(board \\ @board, which, column_index) do
+    if Enum.at(board, 0) |> Enum.at(column_index) !== 0 do
+      raise "That move is not allowed"
+    end
+
+    IO.inspect board
+    |> Enum.reverse
+    |> drop(which, column_index, false, [])
+  end
+
+  defp drop([], _, _, _, board), do: board
+  defp drop([row | rest], which, column_index, true, board) do
+    drop(rest, which, column_index, true, [row | board])
+  end
+  defp drop([row | rest], which, column_index, false, board) do
+    case Enum.at(row, column_index) do
+      0 ->
+        drop(rest, which, column_index, true,
+          [List.replace_at(row, column_index, which) | board])
+      _ ->
+        drop(rest, which, column_index, false, [row | board])
+    end
+  end
+
+  def print_winner(name, which) do
     IO.puts [IO.ANSI.clear]
+    IO.puts []
+    IO.puts []
+    IO.puts [IO.ANSI.red, "And the winner is ..."]
+    IO.puts []
+    IO.puts [@contender_colors[which], name]
+    IO.puts []
+  end
+
+  def print_contenders(contender1, contender2) do
+    IO.puts [IO.ANSI.clear]
+    IO.puts [@contender_colors[1], contender1]
+    IO.puts []
+    IO.puts [IO.ANSI.yellow, "    vs    "]
+    IO.puts []
+    IO.puts [@contender_colors[2], contender2]
+    IO.puts []
+    IO.puts [IO.ANSI.reset]
+  end
+
+  def print_drop(board \\ @board, contender, column) do
+    IO.puts [IO.ANSI.clear]
+    IO.puts Enum.map(1..7, fn
+      ^column -> @contender_characters[contender]
+      n -> @contender_characters[0]
+    end)
+    print(board, false)
+  end
+
+  def print(board \\ @board, clear \\ true) do
+    if clear, do: IO.puts [IO.ANSI.clear]
     for line <- board do
       IO.puts Enum.map(line, fn space ->
-        case space do
-           0 -> ["  "]
-           1 -> [IO.ANSI.cyan, "\xF0\x9F\x85\xB0 "]
-           #1 -> [IO.ANSI.cyan, "O "]
-           2 -> [IO.ANSI.red, "\xF0\x9F\x85\xB1 "]
-           #2 -> [IO.ANSI.red, "O "]
-         end
+        @contender_characters[space]
       end) |>
       List.flatten
     end
     IO.puts [IO.ANSI.reset]
   end
 end
-
-
-
-
-    #status = [IO.ANSI.yellow, "\033[2J", "\xF0\x9F\x98\x81", IO.ANSI.reset]
-     #status = [IO.ANSI.yellow, "\033[<2>A", "\xF0\x9F\x98\x81", IO.ANSI.reset]
-      # IO.puts ["The status is: ", status]
-      #end
-      #end
---
+++ lib/controller.ex
@@ -0,0 +1,41 @@
+defmodule ConnectFour.Controller do
+  alias ConnectFour.Board
+
+  def start_game(opponent1, opponent2) do
+    opp1 = Enum.random([opponent1, opponent2])
+    opp2 = Enum.filter([opponent1, opponent2], fn x -> x !== opp1 end)
+
+    # announcement
+    Board.print_contenders(opp1.call(:name), opp2.call(:name))
+
+    new_board = Board.new()
+    loop(new_board, [opp1, opp2], 0)
+  end
+
+  defp loop(board, opponents = [opp1, opp2], which) do
+    Board.print(board)
+    :timer.sleep(500)
+
+    {:ok, column} = opponents[which].call({
+      :move,
+      case which do
+        0 -> board
+        1 -> Board.flip(board)
+      end
+    })
+
+    Board.print_drop(board, which, column)
+    :timer.sleep(500)
+    new_board = Board.drop(board, which, column)
+    Board.print(new_board)
+    :timer.sleep(500)
+
+    case Board.evaluate_board(new_board) do
+      {:winner, highlight_coords} ->
+        Board.print_winning_board(new_board, highlight_coords)
+        Board.print_winner(opponents[which].call(:name), which)
+      _ ->
+        loop(new_board, opponents, rem(which + 1, 2))
+    end
+  end
+end
