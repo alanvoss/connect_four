10ec125d8da2d79d26bc89bf6b25396e1662ebc0
Alan Voss <avoss@rent.com>
2017-04-27 16:47:00 -0500

    improving the documentaiton on the board helper and rearranging functions to most important at top

--- lib/connect_four/board_helper.ex
+++ lib/connect_four/board_helper.ex
@@ -1,4 +1,9 @@
 defmodule ConnectFour.BoardHelper do
+  @moduledoc """
+    Helpers for evaluating, creating, and appending to boards
+  """
+
+  @doc "Create a new board."
   def new do
     for i <- 0..5 do
       for j <- 0..6 do
@@ -7,6 +12,10 @@ defmodule ConnectFour.BoardHelper do
     end
   end
 
+  @doc """
+    If necessary, flip the 1s and 2s in a board so that everyone receives a board
+    with 1 as themselves and 2 as their opponent.
+  """
   def flip(board) do
     for row <- board do
       for column <- row do
@@ -19,19 +28,51 @@ defmodule ConnectFour.BoardHelper do
     end
   end
 
+  @doc "Evaluate a board and return a winner."
   def evaluate_board(board) do
     evaluate_rows(board) ||
-    evaluate_columns(board) ||
-    evaluate_diagonals(board) ||
-    evaluate_tie(board)
+      evaluate_columns(board) ||
+      evaluate_diagonals(board) ||
+      evaluate_tie(board)
   end
 
+  @doc """
+    Return the value of what is at a particular space on the board (zero indexed).
+      0 - no piece
+      1 - contender 1
+      2 - contender 2
+  """
   def at_coordinate(board, {column, row}) do
     board
     |> Enum.at(row)
     |> Enum.at(column)
   end
 
+  @doc "Does a coordinate fall within the bounds of the board?"
+  def is_valid_coordinate?({column_index, row_index}) do
+    row_index >= 0 &&
+      row_index <= 5 &&
+      column_index >= 0 &&
+      column_index <= 6
+  end
+
+  @doc """
+    Drop a piece of contender (1 or 2) into an existing board at the column and
+    return a new board.
+  """
+  def drop(board, contender, column_index) do
+    if Enum.at(board, 0) |> Enum.at(column_index) !== 0 do
+      {:error, "non-allowed move"}
+    else
+      {:ok,
+        board
+        |> Enum.reverse
+        |> drop(contender, column_index, false, [])
+      }
+    end
+  end
+
+  @doc "Evaluate horizontal winners."
   def evaluate_rows(board) do
     result =
       board
@@ -46,6 +87,7 @@ defmodule ConnectFour.BoardHelper do
     end
   end
 
+  @doc "Evaluate vertical winners."
   def evaluate_columns(board) do
     rotated_board = 
       board
@@ -68,6 +110,7 @@ defmodule ConnectFour.BoardHelper do
     end
   end
 
+  @doc "Evaluate diagonal winners."
   def evaluate_diagonals(board) do
     coordinates =
       board
@@ -91,6 +134,22 @@ defmodule ConnectFour.BoardHelper do
     end
   end
 
+  @doc """
+    Evaluate whether there is any space left to put a piece.  Should only be run
+    after all the other evaluation functions.
+  """
+  def evaluate_tie(board) do
+    found_zero =
+      board
+      |> Enum.find(&(Enum.any?(&1, fn piece -> piece == 0 end)))
+
+    case found_zero do
+      nil -> :tie
+      _ -> nil
+    end
+  end
+
+  @doc "Get the list of all the sets of valid diagonals at least four spaces in width"
   def get_diagonal_coordinates(board) do
     up_and_to_right =
       for row_index <- 3..8 do
@@ -107,6 +166,7 @@ defmodule ConnectFour.BoardHelper do
     up_and_to_right ++ up_and_to_left
   end
 
+  @doc "Construct a diagonal set of coordinates."
   defp diagonal(row_index, column_index, increment) do
     diagonal(row_index, column_index, increment, [])
   end
@@ -115,24 +175,7 @@ defmodule ConnectFour.BoardHelper do
     diagonal(row_index - 1, column_index + increment, increment, acc ++ [{column_index, row_index}])
   end
 
-  def is_valid_coordinate?({column_index, row_index}) do
-    row_index >= 0 &&
-      row_index <= 5 &&
-      column_index >= 0 &&
-      column_index <= 6
-  end
-
-  def evaluate_tie(board) do
-    found_zero =
-      board
-      |> Enum.find(&(Enum.any?(&1, fn piece -> piece == 0 end)))
-
-    case found_zero do
-      nil -> :tie
-      _ -> nil
-    end
-  end
-
+  @doc "Find a place in a list of values where a string of 4 identical non-0s occurs."
   defp find_start_coordinate({row, row_index}) do
     find_start_coordinate(row, {0, row_index}, nil, 0, 0)
   end
@@ -150,18 +193,7 @@ defmodule ConnectFour.BoardHelper do
     find_start_coordinate(rest, {index, row_index}, first, index + 1, 1)
   end
 
-  def drop(board, contender, column_index) do
-    if Enum.at(board, 0) |> Enum.at(column_index) !== 0 do
-      {:error, "non-allowed move"}
-    else
-      {:ok,
-        board
-        |> Enum.reverse
-        |> drop(contender, column_index, false, [])
-      }
-    end
-  end
-
+  @doc "Perform the mutation of dropping a piece onto the board."
   defp drop([], _, _, _, board), do: board
   defp drop([row | rest], contender, column_index, true, board) do
     drop(rest, contender, column_index, true, [row | board])
