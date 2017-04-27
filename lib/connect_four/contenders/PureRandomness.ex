4630850455f121eb0f8027723fc2e21d4ed543a2
Alan Voss <avoss@rent.com>
2017-04-27 01:46:03 -0500

    adding a random opponent that everyone should be able to beat

---
+++ lib/connect_four/contenders/PureRandomness.ex
@@ -0,0 +1,36 @@
+defmodule ConnectFour.Contenders.PureRandomness do
+  use GenServer
+
+  def start_link(default) do
+    GenServer.start_link(__MODULE__, default)
+  end
+
+  def name(pid) do
+    GenServer.call(pid, :name)
+  end
+
+  def move(pid, board) do
+    GenServer.call(pid, {:move, board})
+  end
+
+  def handle_call(:name, _from, state) do
+    letters = for n <- ?A..?Z, do: n
+    random_name =
+      for i <- 1..12 do
+        Enum.random(letters)
+      end
+    {:reply, List.to_string(random_name), state}
+  end
+
+  def handle_call({:move, board}, _from, state) do
+    random_column =
+      board
+      |> Enum.at(0)
+      |> Enum.with_index
+      |> Enum.filter(&(elem(&1, 0) == 0))
+      |> Enum.map(&(elem(&1, 1)))
+      |> Enum.random
+
+    {:reply, random_column, state}
+  end
+end
