13b12526cd21e7bb3d35ee33dfc0df60a214303a
Alec Hansen <alech@companykitchen.com>
2017-05-04 19:55:20 -0500

    random moves but slightly better strategy

--- lib/connect_four/contenders/team_not_cheaters.ex
+++ lib/connect_four/contenders/team_not_cheaters.ex
@@ -1,5 +1,6 @@
 defmodule ConnectFour.Contenders.TeamNotCheaters do
   use GenServer
+  alias ConnectFour.BoardHelper
 
   def start(default) do
     GenServer.start(__MODULE__, %{moved: false})
@@ -10,13 +11,46 @@ defmodule ConnectFour.Contenders.TeamNotCheaters do
   end
 
   def handle_call({:move, board}, _from, state) do
-    
-    if (!state[:moved]) do
-        state = Map.put(state, :moved, true)
-        {:reply, 6, state}
-    else
-        {:reply, 6, state}
+    we_win = check_if_we_can_win(board)
+    they_win = check_if_they_can_win(board)
+    random_move = random_move(board, state)
+
+    cond do
+        !state[:moved] -> 
+                    state = Map.put(state, :moved, true)
+                    {:reply, 4, state}
+        we_win -> {:reply, we_win, state}
+        they_win -> {:reply, they_win, state}               
+        true -> random_move
     end
+
+  end
+
+  defp check_if_they_can_win(board) do
+      opponents_board = BoardHelper.flip(board)
+      check_if_we_can_win(opponents_board)
+  end
+
+  defp random_move(board, state) do
+      random_column =
+        board
+        |> Enum.at(0)
+        |> Enum.with_index
+        |> Enum.filter(&(elem(&1, 0) == 0))
+        |> Enum.map(&(elem(&1, 1)))
+        |> Enum.random 
+        {:reply, random_column, state}
+  end
+
+  defp check_if_we_can_win(board) do  
+    Enum.map(0..6, fn i ->
+        BoardHelper.drop(board,1, i)
+    end)
+    |> Enum.with_index
+    |> Enum.filter(&(&1 != nil))
+    |> Enum.map(&(elem(&1, 1)))
+    |> List.first()
+
   end
 
 end
