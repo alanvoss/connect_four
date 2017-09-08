ef16c82f0111ce46d360e86c0aeec61b11c6eb93
Jade McGough <jade@thezets.com>
2017-09-07 19:57:40 -0700

    working random implementation

---
+++ lib/connect_four/contenders/meow_mix.ex
@@ -0,0 +1,23 @@
+defmodule ConnectFour.Contenders.MeowMix do
+  use GenServer
+
+  def start(default) do
+    GenServer.start(__MODULE__, default)
+  end
+
+  def handle_call(:name, _from, state) do
+    {:reply, "MeowMix9000", state}
+  end
+
+  def handle_call({:move, board}, _from, state) do
+    random_column =
+      board
+      |> Enum.at(0)
+      |> Enum.with_index
+      |> Enum.filter(&(elem(&1, 0) == 0))
+      |> Enum.map(&(elem(&1, 1)))
+      |> Enum.random
+
+    {:reply, random_column, state}
+  end
+end
